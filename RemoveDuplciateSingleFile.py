00b19229190fab4e64dfb607e4591b83f10482d1
Bryce Hazen Howard <39276420+brycehazen@users.noreply.github.com>
2023-03-10 13:39:00 -0500

    Add files via upload

--- RemoveDuplciateSingleFile.py
+++ RemoveDuplciateSingleFile.py
@@ -2,9 +2,14 @@ import pandas as pd
 import glob
 import os
 
-df = pd.read_csv("C:/Users/bhoward/Desktop/Stoof/Input/MASTER - 2022 All Parishes Information .csv" , encoding = "ISO-8859-1")
+df = pd.read_csv("C:/Users/bhoward/Desktop/Stoof/Input/BLANK_PR.CSV" , encoding='latin-1', low_memory=False)
 
-df_new = df.drop_duplicates(subset=['ConsID'])
+# df['CnAdrPrf_Addrline1_EDIT'] = df['CnAdrPrf_Addrline1'].str.strip()
+# df['CnAdrPrf_Addrline1_EDIT'] = df['CnAdrPrf_Addrline1'].str.lower()
+
+# df["CnAdrPrf_Addrline1_EDIT"] = df['CnAdrPrf_Addrline1_EDIT'].str.replace('[^\w\s]','')
+
+df_new = df.drop_duplicates(subset=['Constituent ID'])
     
-df_new.to_csv( "C:/Users/bhoward/Desktop/Stoof/Output/DupesRemoved.csv", index=False )
+df_new.to_csv( "C:/Users/bhoward/Desktop/Stoof/Output/EXPORTBLANK_PR.csv", index=False )
 
