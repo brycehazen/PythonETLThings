c12bdc5dcdbade4e2fc88849c2040e5d91e06d09
brycehazen <39276420+brycehazen@users.noreply.github.com>
2023-08-15 07:09:39 -0400

    Update D1NewAdds.py
    Fixed title logic to use .at instead of .loc

--- DataCleanUp/D1import/D1NewAdds.py
+++ DataCleanUp/D1import/D1NewAdds.py
@@ -3,58 +3,75 @@ import os
 import re
 import gender_guesser.detector as gender
 
+# Function to split first names that have middle names or initials
 def split_first_name(name):
-    """
-    Split a name of the form 'John D' or 'John D.' or 'John David' into first and middle components.
-    Returns a tuple (first, middle).
-    """
+    # Check for format "John D" or "John D."
     match = re.match(r'(\w+)\s([A-Z]\.?)$', name)
     if match:
         return match.group(1), match.group(2)
     
+    # Check for format "John David"
     match = re.match(r'(\w+)\s(\w+)$', name)
     if match:
         return match.group(1), match.group(2)
 
+    # Return the original name if no match is found
     return name, None
 
+# Function to guess gender using the gender_guesser library
 def guess_gender(name):
     d = gender.Detector()
-    return d.get_gender(name)
+    g = d.get_gender(name)
+    
+    # Mapping the gender-guesser outputs to desired values
+    gender_map = {
+        "male": "Male",
+        "mostly_male": "Male",
+        "female": "Female",
+        "mostly_female": "Female",
+        "unknown": "Unknown",
+        "andy": "Ambiguous"
+    }
+    
+    # Return the mapped gender or "Unknown" if not in the map
+    return gender_map.get(g, "Unknown")
 
-# Get the current directory where the script is located
+# Get the directory of the current script
 directory = os.path.dirname(os.path.realpath(__file__))
-
 # List all CSV files in the directory
 csv_files = [f for f in os.listdir(directory) if f.endswith('.CSV') or f.endswith('.csv')]
 
+# Process each CSV file
 for file in csv_files:
-    # Construct the full file path
     file_path = os.path.join(directory, file)
     
     # Read the CSV file into a DataFrame
     df = pd.read_csv(file_path)
     
-    # Clean phone column
+    # Clean phone numbers that have no digits
     df['phone'] = df['phone'].apply(lambda x: '' if all(ch not in '0123456789' for ch in str(x)) else x)
     
     # Split first names and update 'first' and 'middle' columns
     df['first'], df['middle'] = zip(*df['first'].apply(split_first_name))
     
-    # Guess gender for each first name and insert it after 'title' column
+    # Guess and assign gender based on first name
     df['gender'] = df['first'].apply(guess_gender)
+
+    # Explicitly fill in titles based on gender
+    for idx, row in df.iterrows():
+        if pd.isna(row['title']) or row['title'] == '':
+            if row['gender'] == 'Male':
+                df.at[idx, 'title'] = 'Mr.'
+            elif row['gender'] == 'Female':
+                df.at[idx, 'title'] = 'Ms.'
+
+    # Reorder columns to place 'gender' after 'title'
     column_order = df.columns.tolist()
     gender_index = column_order.index('title') + 1
     column_order = column_order[:gender_index] + ['gender'] + column_order[gender_index:-1]
     df = df[column_order]
     
-    # fill in empty titles based on gender
-    df.loc[(df['title'] == '') & (df['gender'] == 'Male'), 'title'] = 'Mr.'
-    df.loc[(df['title'] == '') & (df['gender'] == 'Female'), 'title'] = 'Ms.'
-
-    # Construct the output filename with "_clean" appended
+    # Construct the output filename with "_clean" appended and save the cleaned dataframe
     output_filename = os.path.splitext(file)[0] + '_clean.CSV'
     output_filepath = os.path.join(directory, output_filename)
-    
-    # Save the cleaned dataframe back to a CSV
     df.to_csv(output_filepath, index=False)
