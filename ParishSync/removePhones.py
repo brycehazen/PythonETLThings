add23d079e59cbb9095b5be380e83dd5e69047aa
brycehazen <39276420+brycehazen@users.noreply.github.com>
2023-09-27 02:21:58 -0400

    Update removePhones.py

--- ParishSync/removePhones.py
+++ ParishSync/removePhones.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import os
 
-# Check if redataWithExceptions.csv exists
-if os.path.exists("updated_RE_DataithExceptions.csv"):
-    exceptions = pd.read_csv("updated_RE_DataWithExceptions.csv", encoding='ISO-8859-1')
+# Initialize phone_cons_id outside of the conditional check to avoid NameError later
+phone_cons_id = pd.DataFrame()
+
+# Check if RE_DataWithExceptions.csv exists
+if os.path.exists("RE_DataWithExceptions.csv"):
+    exceptions = pd.read_csv("RE_DataWithExceptions.csv", encoding='ISO-8859-1')
     
     # Filter rows based on conditions
     condition = (exceptions['Column'] == 'PhoneType') & (exceptions['Reason'].str.contains('Error saving record: A duplicate phone number and phone type exists.'))
@@ -16,12 +19,17 @@ if os.path.exists("updated_RE_DataithExceptions.csv"):
         new_rows.append({'ConsID': row['ConsID'], 'Phone': phone})
     
     # Read the phone_cons_id data
-    phone_cons_id = pd.read_csv("PhoneConsID.csv", encoding='ISO-8859-1')
+    if os.path.exists("PhoneConsID.csv"):
+        phone_cons_id = pd.read_csv("PhoneConsID.csv", encoding='ISO-8859-1')
     phone_cons_id = pd.concat([phone_cons_id, pd.DataFrame(new_rows)], ignore_index=True)
     phone_cons_id.drop_duplicates(inplace=True)
     phone_cons_id.to_csv("PhoneConsID.csv", index=False)
 
-# Read datasets
+# Make sure PhoneConsID.csv exists
+if os.path.exists("PhoneConsID.csv"):
+    phone_cons_id = pd.read_csv("PhoneConsID.csv", encoding='ISO-8859-1')
+
+# Read RE_Data.csv
 redata = pd.read_csv("RE_Data.csv", encoding='ISO-8859-1')
 phone_cons_id['Phone'] = phone_cons_id['Phone'].astype(str)
 
