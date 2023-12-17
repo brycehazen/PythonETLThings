be2faece6fabf32ca9733caf0071f1c7dac04d64
brycehazen <39276420+brycehazen@users.noreply.github.com>
2023-12-17 17:57:20 -0500

    Update and rename RemoveDuplciateSingleFile.py to RemoveAllCsvFiles.py

---
+++ RemoveAllCsvFiles.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import glob
+import os
+
+# Get the directory where the script is located
+script_dir = os.path.dirname(__file__)
+
+# Find all CSV files in the directory
+csv_files = glob.glob(os.path.join(script_dir, '*.csv'))
+
+for file in csv_files:
+    # Read the CSV file
+    df = pd.read_csv(file, encoding='ISO-8859-1', low_memory=False)
+
+    # Uncomment and use the following lines if needed for data processing
+    # df['CnAdrPrf_Addrline1_EDIT'] = df['CnAdrPrf_Addrline1'].str.strip()
+    # df['CnAdrPrf_Addrline1_EDIT'] = df['CnAdrPrf_Addrline1'].str.lower()
+    # df["CnAdrPrf_Addrline1_EDIT"] = df['CnAdrPrf_Addrline1_EDIT'].str.replace('[^\w\s]','')
+
+    # Drop duplicates
+    df_new = df.drop_duplicates(subset=['Constituent ID'])
+
+    # Define the output file name
+    output_file_name = os.path.splitext(os.path.basename(file))[0] + '_PROCESSED.csv'
+    output_file = os.path.join(script_dir, output_file_name)
+
+    # Save to CSV
+    df_new.to_csv(output_file, index=False, encoding='ISO-8859-1')
